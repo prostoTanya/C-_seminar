e965451ae6e8f7df155b883719746ae38f4940a4
unknown <tyakovleva2014@gmail.com>
2022-10-25 01:03:46 +0300

    Добавила Project4

---
+++ Project4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int numberUser = Convert.ToInt32(Console.ReadLine());
+int numberA = numberUser - numberUser * 2;
+int number = numberA;
+int count = 0;
+
+Console.WriteLine(numberA);
+
+while(number < numberUser)
+{
+    number = number + 1;
+    count = count + 1;
+    Console.WriteLine(number);
+} 
