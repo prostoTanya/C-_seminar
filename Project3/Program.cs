e154c039f9dc00b2f47283290425637f129d3658
unknown <tyakovleva2014@gmail.com>
2022-10-25 00:55:28 +0300

    Добавила else if

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -6,32 +6,32 @@ if(userNumber == 1)
     Console.WriteLine("День 1 - Понедельник");
 }
 
-if(userNumber == 2)
+else if(userNumber == 2)
 {
     Console.WriteLine("День 2 - Вторник");
 }
 
-if(userNumber == 3)
+else if(userNumber == 3)
 {
     Console.WriteLine("День 3 - Среда");
 }
 
-if(userNumber == 4)
+else if(userNumber == 4)
 {
     Console.WriteLine("День 4 - Четверг");
 }
 
-if(userNumber == 5)
+else if(userNumber == 5)
 {
     Console.WriteLine("День 5 - Пятница");
 }
 
-if(userNumber == 6)
+else if(userNumber == 6)
 {
     Console.WriteLine("День 6 - Суббота");
 }
 
-if(userNumber == 7)
+else if(userNumber == 7)
 {
     Console.WriteLine("День 7 - Воскресенье");
 }
