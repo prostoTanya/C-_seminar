eace745ca38d58fac6eb7daf167b6b22cefcb698
unknown <tyakovleva2014@gmail.com>
2022-10-25 00:40:41 +0300

    Добавила Project3

---
+++ Project3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if(userNumber == 1)
+{
+    Console.WriteLine("День 1 - Понедельник");
+}
+
+if(userNumber == 2)
+{
+    Console.WriteLine("День 2 - Вторник");
+}
+
+if(userNumber == 3)
+{
+    Console.WriteLine("День 3 - Среда");
+}
+
+if(userNumber == 4)
+{
+    Console.WriteLine("День 4 - Четверг");
+}
+
+if(userNumber == 5)
+{
+    Console.WriteLine("День 5 - Пятница");
+}
+
+if(userNumber == 6)
+{
+    Console.WriteLine("День 6 - Суббота");
+}
+
+if(userNumber == 7)
+{
+    Console.WriteLine("День 7 - Воскресенье");
+}
