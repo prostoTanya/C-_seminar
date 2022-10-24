386a3dea7d819314c2aa7036b0e460cf0241b8c0
unknown <tyakovleva2014@gmail.com>
2022-10-25 00:20:36 +0300

    Добавила Project2

---
+++ Project2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numberUser1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberUser2 = Convert.ToInt32(Console.ReadLine());
+int square = numberUser2 * numberUser2;
+
+if (square == numberUser1)
+{
+    Console.WriteLine($"Число {numberUser1} является квадратом числа {numberUser2}");
+} 
+else
+{
+  Console.WriteLine($"Число {numberUser1} не является квадратом числа {numberUser2}");  
+}
