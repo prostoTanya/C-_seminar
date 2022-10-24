82b401f0bc997ded11f5e714287a6baf30ad9397
unknown <tyakovleva2014@gmail.com>
2022-10-24 23:54:34 +0300

    Создала Project1

---
+++ Project1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber * userNumber;
+Console.WriteLine($"Квадрат числа {userNumber} равен {result}");
